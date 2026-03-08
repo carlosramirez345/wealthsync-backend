ee056ea7f7ae084efc51a2f99d449d8589ccb226
carlosramirez234 <carlos.ramirez.55046@gmail.com>
2026-03-07 19:10:59 -0600

    cambio a connection stringe

--- src/config/db.js
+++ src/config/db.js
@@ -5,13 +5,9 @@ import dotenv from 'dotenv';
 // Cargamos las variables de tu archivo .env
 dotenv.config();
 
-// Creamos un "Pool" de conexiones a la base de datos
+// Creamos un "Pool" de conexiones a la base de datos usando la llave maestra
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL
 });
 
 pool.on('connect', () => {
